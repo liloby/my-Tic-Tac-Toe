68ed3a3305741375698cc151464dd41a480bfbc5
Oby <obyli214@gmail.com>
2022-06-02 01:08:13 -0700

    added select restrictions..still needs win conditions

--- js/main.js
+++ js/main.js
@@ -1,60 +1,76 @@
 /*----- constants -----*/
-// const win cell 1,2,3. 4,5,6. 7,8,9. 1,4,7. 2,5,8. 3,6,9. 1,5,9. 3,5,7.
-const boardEl = document.getElementById("board");
-const cellEls = document.querySelectorAll(".cell");
-const resetEl = document.getElementById("resetButton");
-const turnEl = document.getElementById("turn")
-const maxTurn = 9;
+const player = {
+  1: "red",
+  "-1": "blue",
+  null: "white",
+};
+
+const winCombos = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+];
 
 /*----- app's state (variables) -----*/
 let turn = 0;
-let boardData = [
-[0, 0, 0],
-[0, 0, 0],
-[0, 0, 0]
-]
-
+let winner;
+let board;
 /*----- cached element references -----*/
+const boardEl = document.getElementById("board");
+const cellEls = document.querySelectorAll(".cell");
+const resetEl = document.getElementById("resetButton");
+const turnEl = document.getElementById("turn");
 
 /*----- event listeners -----*/
 boardEl.addEventListener("click", handleClick);
-resetEl.addEventListener("click", resetGame);
+resetEl.addEventListener("click", initGame);
 /*----- functions -----*/
 
 function handleClick(evt) {
-  while (turn < maxTurn) {
+  if (evt.target.innerText === "") {
     turn += 1;
     if (turn % 2 === 0) {
       evt.target.innerText = "X";
-      evt.target.style.color = 'rgb(53, 27, 181)'
+      evt.target.style.color = "blue";
       evt.target.classList.add("x");
-      turnEl.innerText= "O's Turn";
-      turnEl.style.color = 'rgb(239, 177, 177)'
+      turnEl.innerText = "O's Turn";
+      turnEl.style.color = "rgb(200, 28, 28)";
     } else {
       evt.target.innerText = "O";
-      evt.target.style.color = 'rgb(239, 177, 177)'
+      evt.target.style.color = "rgb(200, 28, 28)";
       evt.target.classList.add("o");
-      turnEl.innerText= "X's Turn";
-      turnEl.style.color = 'rgb(53, 27, 181)'
+      turnEl.innerText = "X's Turn";
+      turnEl.style.color = "blue";
     }
-    return
+    
+    // winCombos.forEach(function(combo) {
+    //   if (cellEls[combo[0]].innerHTML === cellEls[combo[1]].innerHTML && cellEls.innerHTML !== "") {
+    //     console.log("Win")
+    //   }
+    // })
+
+    if (turn === 9) {
+      turnEl.innerText = "Good Game!";
+      turnEl.style.color = 'gold'
+    }
+    return;
   }
+  return;
 }
 
 
-function resetGame(evt) {
+function initGame(evt) {
   cellEls.forEach(function (cell) {
     cell.innerText = " ";
-    cell.classList.remove('o')
-    cell.classList.remove('x')
+    cell.classList.remove("o");
+    cell.classList.remove("x");
   });
   turn = 0;
-  boardData = [
-    [0, 0, 0],
-    [0, 0, 0],
-    [0, 0, 0]
-    ]
-    turnEl.innerText= "O's  Turn";
-    turnEl.style.color = 'rgb(239, 177, 177)'
+  turnEl.innerText = "O's  Turn";
+  turnEl.style.color = "rgb(200, 28, 28)";
 }
-
